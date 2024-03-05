5236169e8f8cc77dbc7cd74f384499cb0d3f30e7
DESKTOP-O3LMC10\User <jungmiP0330@gmail.com>
2024-03-05 17:58:09 +0900

    add comment

--- java-basic/src/kr/ac/kopo/day01/HelloMain.java
+++ java-basic/src/kr/ac/kopo/day01/HelloMain.java
@@ -1,9 +1,19 @@
 package kr.ac.kopo.day01;
-
+/**
+ * 문서화 주석 만들기
+ * 
+ * @author 만든 사람 
+ * @version 0.1
+ *  
+ */
 public class HelloMain {
+	
+	/**
+	 * 실행하기 위한 메소드
+	 * @param args
+	 * */
 
 	public static void main(String[] args) {
-	
 		
 		System.out.print(1); //라인없이
 		System.out.println(true); // 새 라인
